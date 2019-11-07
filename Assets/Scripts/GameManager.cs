ca9d0f214b02df3c052b91d672e974af06cd880d
DiegoKiller98 <43404893+DiegoKiller98@users.noreply.github.com>
2019-11-07 16:33:01 +0100

    Menu added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [Tooltip("Only applies at Scene1")]
     [SerializeField] float timeLeft;
+
+    [Tooltip("Only applies at MenuScene")]
+    [SerializeField] Button jugar;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        if(SceneManager.GetActiveScene().name == "MenuScene")
+        {
+            Button b = jugar.GetComponent<Button>();
+            b.onClick.AddListener(loadScene);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime;
+        if (SceneManager.GetActiveScene().name == "Scene1") {
+            timeLeft -= Time.deltaTime;
 
-        this.GetComponentInChildren<UnityEngine.UI.Text>().text = "Time left: " + timeLeft.ToString("F1");
+            this.GetComponentInChildren<Text>().text = "Time left: " + timeLeft.ToString("F1");
 
-        if (timeLeft < 0)
-        {
-            SceneManager.LoadScene("Scene2");
+            if (timeLeft < 0)
+            {
+                SceneManager.LoadScene("Scene2");
+            }
         }
+    }
+
+    void loadScene()
+    {
+        SceneManager.LoadScene("Scene1");
     }
 }
