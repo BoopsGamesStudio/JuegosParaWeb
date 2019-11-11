6f4b89c76ed8a30624917400ba0eee8e4f899183
DiegoKiller98 <43404893+DiegoKiller98@users.noreply.github.com>
2019-11-11 18:56:33 +0100

    Cambios varios

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x, player.transform.position.y + 15, this.transform.localPosition.z);
+    }
+}
